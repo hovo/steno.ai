1dbbb0e87516957bb92f01cbc9c191abcfd80a2b
Hovo <hovo.khc@gmail.com>
2019-05-05 19:08:22 -0400

    Added view component for transcribing

---
+++ frontend/src/views/transcribe.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="container" style="display: flex; min-height: 100vh; flex-direction: column;">
+    <div class="container" style="display: flex; min-height: 100vh; flex-direction: column;">
+        <section class="articles" style="flex:1; overflow:auto;">
+            <div class="column is-4 is-offset-6">
+                <!-- <div class="field">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Normal input">
+                    </div>
+                </div> -->
+            </div>
+            <div class="column is-8 is-offset-2">
+                <h1 class="title" style="color: #022144">Recording.wav</h1>
+                <h6 class="subtitle is-6">
+                    Sun 5/5 - 12:40AM
+                </h6>
+                <hr class="hr">
+                <span style="color: #1B2733">
+                    Creating a Single-page Application with Vue + Vue Router is dead simple. With Vue.js,
+                    we are already composing our application with components. When adding Vue Router to the mix, all we need to do is 
+                    map our components to the routes and let Vue Router know where to render them. Here's a basic example:
+                </span>
+            </div>
+        </section>
+        <footer>
+            <div class="content">
+                <div class="column is-8 is-offset-2">
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'transcribe',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
