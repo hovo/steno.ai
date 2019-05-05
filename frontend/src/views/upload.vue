9da19e730527344d6e9c87f720f46f7dcf4446b8
Hovo <hovo.khc@gmail.com>
2019-05-05 16:13:07 -0400

    Updated file upload view layout

--- frontend/src/views/upload.vue
+++ frontend/src/views/upload.vue
@@ -1,5 +1,15 @@
 <template>
-    <p>Hello World</p>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-8 is-offset-2">
+                    <img src="../assets/file_upload.png">
+                    <h5 class="title is-5">Upload an audio file to transcribe</h5>
+                    <h6 class="subtitle is-6">.wav, .mp3, .m4a, .flac – up to 20 MB</h6>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
