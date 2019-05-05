60b24f6f3ca1b59350dd3571f0bece1b741f1606
Hovo <hovo.khc@gmail.com>
2019-05-05 15:54:08 -0400

    Initialized root route

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from '@/router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
-Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ frontend/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Upload'
+        }
+    ]
+})
+
+export default router
