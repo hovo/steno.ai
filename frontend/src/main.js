14ab047fd20ff78692f389ae8c0ebbb329ff9892
Hovo <hovo.khc@gmail.com>
2019-05-05 19:51:50 -0400

    Added state management library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,12 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
 Vue.use(VuePlyr)
+Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
