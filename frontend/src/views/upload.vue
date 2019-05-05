0bc39e11cd168d4abadb4dd4400499181d32b14c
Hovo <hovo.khc@gmail.com>
2019-05-05 16:26:48 -0400

    Added file upload component

--- frontend/src/views/upload.vue
+++ frontend/src/views/upload.vue
@@ -6,6 +6,16 @@
                     <img src="../assets/file_upload.png">
                     <h5 class="title is-5">Upload an audio file to transcribe</h5>
                     <h6 class="subtitle is-6">.wav, .mp3, .m4a, .flac – up to 20 MB</h6>
+                    <b-field class="file" style="display: inline-flex">
+                        <b-upload v-model="file">
+                            <a class="button is-primary">
+                                <span>Upload</span>
+                            </a>
+                        </b-upload>
+                        <span class="file-name" v-if="file">
+                            {{ file.name }}
+                        </span>
+                    </b-field>
                 </div>
             </div>
         </div>
@@ -17,7 +27,7 @@ export default {
     name: 'upload',
     data() {
         return {
-
+            file: null
         }
     }
 }
