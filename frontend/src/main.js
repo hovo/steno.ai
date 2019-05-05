0c2633debe6abd4eda039a4bf666fbc7197aeb74
Hovo <hovo.khc@gmail.com>
2019-05-05 15:41:28 -0400

    Import Vue Router plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
