4a3cd0715a09cabaf3878d7268e37636ad485fed
Hovo <hovo.khc@gmail.com>
2019-05-05 21:31:26 -0400

    Implemented function for sampling rate

--- backend/file_service.py
+++ backend/file_service.py
@@ -1,12 +1,25 @@
+from pydub import AudioSegment
 from google.cloud import storage
 import uuid
 
 GCS_BUCKET_NAME = "steno"
 
+def get_sampling_rate(file):
+    """
+    Returns the sampling rate for the audio file
+    """
+    sampling_rate = AudioSegment.from_file(file, format="wav").frame_rate
+    return sampling_rate
+
 def upload_to_gcs(file):
-    '''
+    """
     Uploads file to Google Cloud Storage
-    '''
+
+    Returns
+    -------
+    string
+        Uploaded file name
+    """
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(GCS_BUCKET_NAME)
 
@@ -14,3 +27,5 @@ def upload_to_gcs(file):
     filename = str(uuid.uuid4()) + "." + file.filename.split(".")[-1]
     blob = bucket.blob(filename)
     blob.upload_from_file(file)
+
+    return filename
