5eabe8b20a4952f6bdd63bdfcec8a0be55ae6eba
Hovo <hovo.khc@gmail.com>
2019-05-05 20:21:12 -0400

    Added file upload route

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, jsonify, request
+from google.cloud import storage
 
 app = Flask(__name__)
 
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    f = request.files['file']
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket("steno")
+    blob = bucket.blob(f.filename)
+    blob.upload_from_file(f)
+
+    return jsonify()
+    
 if __name__ == "__main__":
     app.run(port=8000)
