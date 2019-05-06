440c106da9e8110259d60282782edc6d7dd0580e
Hovo <hovo.khc@gmail.com>
2019-05-06 16:00:38 -0400

    Added documetation

--- backend/store.py
+++ backend/store.py
@@ -2,7 +2,24 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 def cache_metadata(key, val):
+    """
+    Stores audio metadata in Redis
+    Parametrs
+    ---------
+    key
+        string: UUID which uniquely identifies the metadata
+    
+    val
+        string: serialized object containing the audio file metadata
+    """
     r.set(key, val)
 
-def get_metadata(key):
+def get_file_metadata(key):
+     """
+    Returns the metadata from Redis
+    Parametrs
+    ---------
+    key
+        string: UUID which uniquely identifies the metadata
+    """
     return r.get(key)
