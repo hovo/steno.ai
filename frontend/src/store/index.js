38a011027aef91476958fc57d9cc237fe7ad8540
Hovo <hovo.khc@gmail.com>
2019-05-05 20:04:24 -0400

    Defined store state, getters and mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,18 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const store = new Vuex.Store()
+const store = new Vuex.Store({
+    state: {
+        audioFile: null
+    },
+    getters: {
+        audioFile: state => state.audioFile
+    },
+    mutations: {
+        SET_AUDIO_FILE: (state, file) => {
+            state.audioFile = file
+        }
+    }
+})
 
 export default store
