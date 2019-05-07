0c9bf74a9226eee62c5dc9f47c7e2f331dc1d7ac
Hovo <hovo.khc@gmail.com>
2019-05-07 00:27:30 -0400

    Initialized Axios HTTP client library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,10 +6,12 @@ import 'buefy/dist/buefy.css'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 import store from '@/store'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
 Vue.use(VuePlyr)
+Vue.prototype.$http = Axios
 
 new Vue({
   render: h => h(App),
